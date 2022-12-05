7f8bb8743d6443677a49459ffdb4c590019d4c00
angel Sanchez-Panadero <angelasp2001@gmail.com>
2022-12-05 02:28:13 -0600

    linked jquery and javascript files, added a search button and text div to page

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+
+//geocoding api will take a city and convert info into lat and lon
+//fiveDay Forecast needs lat and lon to populate the 5 day forecast
+
+
+
+var searchButton = $('.searchButton');
+var userInput = $('.userCity');
+
+
+
+//var fiveDayForecast = 'api.openweathermap.org/data/2.5/forecast?appid=90f9cf47a9397ccf3fcd315994691698&lat={lat}&lon={lon}';
+
+//var geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?appid=90f9cf47a9397ccf3fcd315994691698';
+
+searchButton.on('click', console.log("hello"));
+
+
+
+/*fetch(fiveDayForecast + userInput)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data)
+  });
+*/
+  
