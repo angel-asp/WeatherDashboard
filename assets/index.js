b148caa888f799cc7813dca3c5dd10a47cf4144d
angel Sanchez-Panadero <angelasp2001@gmail.com>
2022-12-07 17:05:32 -0600

    worked on todays forcast

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,8 @@
 //geocoding api will take a city and convert info into lat and lon
 //fiveDay Forecast needs lat and lon to populate the 5 day forecast
 
-
-
+var currentDay = dayjs().format('MMMM dddd D YYYY');
+ 
 var searchButton = $('#searchButton');
 var userInput = $('.userInput');
 var today = $('#todaysForecast')
@@ -11,14 +11,16 @@ var today = $('#todaysForecast')
 var geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?appid=90f9cf47a9397ccf3fcd315994691698&q=';
 
 
-var fiveDayForecast = 'http:api.openweathermap.org/data/2.5/forecast?appid=90f9cf47a9397ccf3fcd315994691698&units=metric&lat=';
-var currentDayForecast = 'https://api.openweathermap.org/data/2.5/weather?appid=90f9cf47a9397ccf3fcd315994691698&units=metric&lat=';
+var fiveDayForecast = 'http:api.openweathermap.org/data/2.5/forecast?appid=90f9cf47a9397ccf3fcd315994691698&units=imperial&lat=';
+var currentDayForecast = 'https://api.openweathermap.org/data/2.5/weather?appid=90f9cf47a9397ccf3fcd315994691698&units=imperial&lat=';
 
 searchButton.click(getUserInput); 
+searchButton.click(inputFiveday)
 
 function getUserInput() {
   //gets user input and places into var x
-    var x = userInput.val();    
+    var x = userInput.val();   
+    
     //place var x with geocoding api to get city lat and lon
     fetch(geocodingAPI + x)
     .then(function (response) {
@@ -27,8 +29,6 @@ function getUserInput() {
     .then(function (data) {
       var lat = data[0].lat;
       var lon = data[0].lon;
-      console.log(lat);
-      console.log(lon)
       //var lat = data[0].lat;
       return fetch(currentDayForecast + lat + "&lon=" + lon) 
     })
@@ -38,23 +38,53 @@ function getUserInput() {
     .then(function (forecastAPI) {
       //create elements to place in div
       //create name date temp wind and humidity
-      console.log(forecastAPI);
 
       var h1City = document.createElement('h1');
+      var h1Temp = document.createElement('h1');
+      var h1FeelsLike = document.createElement('h1');
+      var h1Wind = document.createElement('h1');
+      var h1Humidity = document.createElement('h1');
 
-      h1City.textContent = forecastAPI.name;
-      console.log(h1City)
-      today[1].appendChild(h1City);
 
-      
-    })
-
-    //use lat and lon to find forecast
-    //display forecast inside divs
-    //save to local stroage
+      h1City.textContent = forecastAPI.name + " " + currentDay;
+      h1Temp.textContent = "temp " + forecastAPI.main.temp + " degrees";
+      h1FeelsLike.textContent = "feels like " + forecastAPI.main.feels_like + " degrees";
+      h1Wind.textContent = "wind speed of " + forecastAPI.wind.speed + " mph";
+      h1Humidity.textContent = "humidity of " + forecastAPI.main.humidity + " %";
     
+      
 
-}
+
+      today.append(h1City);
+      today.append(h1Temp);
+      today.append(h1FeelsLike);
+      today.append(h1Wind);
+      today.append(h1Humidity);
+    }) }
+
+
+
+
+    function inputFiveday() {
+      //gets user input and places into var x
+        var x = userInput.val();  
+
+    fetch(geocodingAPI + x)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      //var lat = data[0].lat;
+      return fetch(fiveDayForecast + lat + "&lon=" + lon) 
+    })
+    .then(function (fiveDayAPI) {
+      return fiveDayAPI.json();
+    })
+    .then(function (fiveDays) {
+      console.log(fiveDays);
+})}
 
 
 
